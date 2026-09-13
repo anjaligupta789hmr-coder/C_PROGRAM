ba030e4885c24972053968236d65d73fd2437af1
Anjali Gupta <anjaligupta789hmr@gmail.com>
2026-09-13 19:17:39 +0530

    Add files via upload

---
+++ ascii_value.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main () {
+    
+    char letter;
+    printf("enter a letter:");
+    scanf("%c",&letter);
+    printf(" ASCII value of %c is %d \n",letter ,letter);
+    return 0;
+    
+}
