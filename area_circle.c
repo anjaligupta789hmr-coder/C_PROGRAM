d220843f52fe5a079a7364d24aa68f0dc8aa90ed
Anjali Gupta <anjaligupta789hmr@gmail.com>
2026-09-13 16:43:55 +0530

    Add files via upload

---
+++ area_circle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main (){
+    float r; 
+    printf("enter a radius:");
+    scanf("%f",&r);
+    printf("area of circle = %f\n",3.14*r*r);
+    printf("circumfence of circle = %f \n",2*3.14*r);
+    return 0;
+
+}
