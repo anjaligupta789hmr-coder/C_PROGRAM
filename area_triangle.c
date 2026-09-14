3a9701f98b914c585501965607569c6f038aa5cd
Anjali Gupta <anjaligupta789hmr@gmail.com>
2026-09-14 22:48:34 +0530

    Add files via upload

---
+++ area_triangle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main (){
+    float b,h;
+    printf("base of triangle:");
+    scanf("%f",&b);
+    printf("height of triangle:");
+    scanf("%f",&h);
+    printf("area of triangle = %f",b*1/2*h);
+    return 0;
+}
